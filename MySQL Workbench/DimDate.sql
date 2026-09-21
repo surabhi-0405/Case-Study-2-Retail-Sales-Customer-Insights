64fd599158f36e8a1850e384bd298b3f80dc8e27
surabhi-0405 <167306630+surabhi-0405@users.noreply.github.com>
2026-09-21 17:30:49 +0530

    Create DimDate table for retail sales data

--- MySQL Workbench/DimDate.sql
+++ MySQL Workbench/DimDate.sql
@@ -1,1 +1,13 @@
+USE retail_sales_dw;
 
+CREATE TABLE IF NOT EXISTS DimDate (
+    DateKey INT PRIMARY KEY,
+    FullDate DATE NOT NULL,
+    Year INT NOT NULL,
+    MonthNumber INT NOT NULL,
+    MonthName VARCHAR(20) NOT NULL,
+    Quarter VARCHAR(2) NOT NULL,
+    Day INT NOT NULL,
+    DayName VARCHAR(20) NOT NULL,
+    DayOfWeek INT NOT NULL
+);
